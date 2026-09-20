d486cb7fb5d0b740e69b3994f09c0f312d292352
makinde46 <makindejeff46@gmail.com>
2026-09-20 08:56:15 +0300

    Initial commit

---
+++ businessapplication.py
@@ -0,0 +1,15 @@
+#Create variables and assign appropriate values
+business_name = "Tech Solutions Ltd"
+product_name = "Laptop"
+product_price = 75000
+quantity_purchased = 5
+
+#Calculate the total purchase cost
+total_cost = product_price * quantity_purchased
+
+#Display the business information
+print("Business Name:", business_name)
+print("Product Name:", product_name)
+print("Quantity Purchased:", quantity_purchased)
+print("Product Price:", product_price)
+print("Total Cost:", total_cost)
